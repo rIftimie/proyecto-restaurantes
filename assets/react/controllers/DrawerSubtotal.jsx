e009683ed61be0dcfcaedc65d02c6bafa8b02e5c
whoisaxx13 <droidermunoz@gmail.com>
2023-03-02 18:09:58 +0100

    Stripe en DrawerSubtotal #7

--- assets/react/controllers/DrawerSubtotal.jsx
+++ assets/react/controllers/DrawerSubtotal.jsx
@@ -9,16 +9,19 @@ function DrawerSubtotal({ isOpen, setIsDrawerOpen }) {
   };
   return (
     <div className={`drawer ${isOpen ? "open" : ""}`}>
-      <div className=" d-flex justify-content-center flex-colum flex-wrap">
+      <div className=" d-flex flex-column col-8 mt-5">
+        <Stripe></Stripe>
         <p>Pago con tarjeta</p>
         <p>Pago en efectivo</p>
         <p>Paypal</p>
       </div>
-      <button
-        onClick={handleCloseDrawer}
-        type="button"
-        className="btn btn-outline-danger m-3"
-      >Cerrar</button>
+      <div className='col-4'>
+        <button
+          onClick={handleCloseDrawer}
+          type="button"
+          className="btn btn-outline-danger m-3"
+        >Cerrar</button>
+      </div>
     </div>
   );
 }
