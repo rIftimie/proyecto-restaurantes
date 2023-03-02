4ce08bd0a4ffada3033733a221a174b8138275d9
Brinqui <arincon2709@gmail.com>
2023-03-02 16:44:03 +0100

    Revert "Components Subtotal y DrawerSubtotal + css"
    This reverts commit 2de5f2997905e355ba0a17c21781ad6d5cc8e516.

--- assets/react/controllers/DrawerSubtotal.jsx
+++ assets/react/controllers/DrawerSubtotal.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 function DrawerSubtotal(props) {
   return (
     <div className={`drawer ${props.isOpen ? 'open' : ''}`}>
-      <p>Pago con tarjeta</p>
       <p>Paypal</p>
-      <p>Pago en efectivo</p>
+      <p>Stripe</p>
     </div>
   )
 }
--- assets/react/controllers/Subtotal.jsx
+++ assets/react/controllers/Subtotal.jsx
@@ -5,12 +5,11 @@ const Subtotal = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const handleOpenDrawer = () => {
-    if(isDrawerOpen){
-      setIsDrawerOpen(false);
-    }else{
-      setIsDrawerOpen(true);
-    }
+    setIsDrawerOpen(true);
   };
+  const handleCloseDrawer = () =>{
+    setIsDrawerOpen(false);
+  }
   return (
     <>
       <div className="container d-flex justify-content-between border-bottom border-dark border-top">
