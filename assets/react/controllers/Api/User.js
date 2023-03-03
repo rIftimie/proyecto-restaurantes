cca1dd023f7a03fab6ebb4dc6f2b07c33f614d9c
jose carlos fernandez molina <josecarlosgoku7@gmail.com>
2023-03-03 21:00:30 +0100

    User bien

--- assets/react/controllers/Api/User.js
+++ assets/react/controllers/Api/User.js
@@ -5,7 +5,6 @@ export const getUsers = async () => {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
         return data;
     } catch (error) {
         console.log(error);
