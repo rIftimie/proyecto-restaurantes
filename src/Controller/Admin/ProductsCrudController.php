8e2824fa2a209ebbdee28764c371d4ca88e95deb
jose carlos fernandez molina <josecarlosgoku7@gmail.com>
2023-03-03 16:32:41 +0100

    fdsafds

--- src/Controller/Admin/ProductsCrudController.php
+++ src/Controller/Admin/ProductsCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Products;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+
 
 class ProductsCrudController extends AbstractCrudController
 {
@@ -12,14 +16,14 @@ class ProductsCrudController extends AbstractCrudController
         return Products::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextField::new('description'),
+            ArrayField::new('allergens'),
+            IntegerField::new('stock'),
         ];
     }
-    */
+    
 }
