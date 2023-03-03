5e37d760c225b198ac8139d9681e6b6ac6894ff8
Hugo Rodriguez <hugororo2003@gmail.com>
2023-03-03 20:55:35 +0100

    Panel de administracion #20

--- assets/react/pages/admin/AdminPages.jsx
+++ assets/react/pages/admin/AdminPages.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import './adminPage.css';
+import Prueba from "../../components/Prueba";
+import User from "../../components/User";
+import Product from "../../components/Product";
+import Order from "../../components/Order";
+import Table from "../../components/Table";
 
-const AdminPages = ()=>{
-    return (
-        <div>Esto es admin</div>
-    );
+const components = {
+  Prueba: <Prueba />,
+  User: <User />,
+  Product: <Product />,
+  Order: <Order />,
+  Table: <Table />,
+};
+
+const AdminPages = () => {
+  const [selectedItem, setSelectedItem] = useState('Prueba');
+
+  const handleClick = (name) => {
+    setSelectedItem(name);
+  };
+
+  return (
+    <div className="admin-container">
+      <h1 className="admin-title">Admin panel</h1>
+      <nav className="admin-nav">
+        <ul>
+          <li className="elemento-menu" onClick={() => handleClick('User')}>User</li>
+          <li className="elemento-menu" onClick={() => handleClick('Product')}>Product</li>
+          <li className="elemento-menu" onClick={() => handleClick('Order')}>Order</li>
+          <li className="elemento-menu" onClick={() => handleClick('Table')}>Table</li>
+        </ul>
+      </nav>
+      <div className="admin-content">{components[selectedItem]}</div>
+    </div>
+  );
 }
 
 export default AdminPages;
