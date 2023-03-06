80ac745c68f045bf484ce5aadfa3bbf0f333ef0f
Hugo Rodriguez <hugororo2003@gmail.com>
2023-03-06 17:51:26 +0100

    Reteo admin panel react #20

--- assets/react/pages/admin/AdminPages.jsx
+++ assets/react/pages/admin/AdminPages.jsx
@@ -15,26 +15,6 @@ const components = {
 };
 
 const AdminPages = () => {
-  const [selectedItem, setSelectedItem] = useState('Prueba');
-
-  const handleClick = (name) => {
-    setSelectedItem(name);
-  };
-
-  return (
-    <div className="admin-container">
-      <h1 className="admin-title">Admin panel</h1>
-      <nav className="admin-nav">
-        <ul>
-          <li className="elemento-menu" onClick={() => handleClick('User')}>User</li>
-          <li className="elemento-menu" onClick={() => handleClick('Product')}>Product</li>
-          <li className="elemento-menu" onClick={() => handleClick('Order')}>Order</li>
-          <li className="elemento-menu" onClick={() => handleClick('Table')}>Table</li>
-        </ul>
-      </nav>
-      <div className="admin-content">{components[selectedItem]}</div>
-    </div>
-  );
 }
 
 export default AdminPages;
