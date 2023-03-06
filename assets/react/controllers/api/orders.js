26a3e41fbf74f4aea0d3863241af5dd5e8273723
ijimsa <israel.js@hotmail.es>
2023-03-06 18:04:29 +0100

    Merge Cloudinary #19

--- assets/react/controllers/api/orders.js
+++ assets/react/controllers/api/orders.js
@@ -1,105 +1,91 @@
-import { async } from "regenerator-runtime";
+import { async } from 'regenerator-runtime';
 
 const url = 'http://127.0.0.1:8000/api/orders';
-const url2= 'http://127.0.0.1:8000/orders';
+const url2 = 'http://127.0.0.1:8000/orders';
 
 export const getOrders = async () => {
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
-    
-   
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const getOrder = async (order) => {
-  
-   try {
-    const response = await fetch(`${url}/${order.id}`);
-    const data = await response.json();
-    return data;
-   } catch (error) {
-    console.log(error);
-   }
-   
-  };
+	try {
+		const response = await fetch(`${url}/${order.id}`);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 export const deleteOrder = async (order) => {
-    try {
-        
-        const response = await fetch(`${url}/${order.id}`, {
-        method: "DELETE",
-        });
+	try {
+		const response = await fetch(`${url}/${order.id}`, {
+			method: 'DELETE',
+		});
 
-    
-        return await response.json();
-        
-    } catch (error) {
-        console.log(error);
-    }
-
+		return await response.json();
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const updateOrder = async (order) => {
-    try {
-    
-        const response = await fetch(`${url}/${order.id}/edit`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json; charset=utf-8" },
-        body: JSON.stringify(order),
-        });
+	try {
+		const response = await fetch(`${url}/${order.id}/edit`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json; charset=utf-8' },
+			body: JSON.stringify(order),
+		});
 
-    
-        return await response.json();
-        
-    } catch (error) {
-        console.log(error);
-    }
+		return await response.json();
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const acceptOrder = async (order) => {
-    try {
-        const data = {};
-        const response = await fetch(`${url2}/kitchen/${order.id}/accept`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json; charset=utf-8" },
-        body: JSON.stringify(order),
-        });
+	try {
+		const data = {};
+		const response = await fetch(`${url2}/kitchen/${order.id}/accept`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json; charset=utf-8' },
+			body: JSON.stringify(order),
+		});
 
-    
-        return response;
-        
-    } catch (error) {
-        console.log(error);
-    }
+		return response;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const finishOrder = async (order) => {
-    try {
-        const response = await fetch(`${url2}/kitchen/${order.id}/finish`,{
-            method: "PUT",
-            headers: { "Content-Type": "application/json; charset=utf-8" },
-            body: JSON.stringify(order),
-        });
-        return response;
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		const response = await fetch(`${url2}/kitchen/${order.id}/finish`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json; charset=utf-8' },
+			body: JSON.stringify(order),
+		});
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-export const declineOrder = async (order) =>{
-    try {
-        const response = await fetch(`${url2}/kitchen/${order.id}/decline`,{
-            method: "PUT",
-            headers: { "Content-Type": "application/json; charset=utf-8" },
-            body: JSON.stringify(order),
-        });
-        return response;
-    } catch (error) {
-        console.error(error);
-    }
-}
+export const declineOrder = async (order) => {
+	try {
+		const response = await fetch(`${url2}/kitchen/${order.id}/decline`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json; charset=utf-8' },
+			body: JSON.stringify(order),
+		});
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+};
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -9,6 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Cloudinary\Cloudinary;
+use Cloudinary\Transformation\Resize;
 
 #[Route('/products')]
 class ProductsController extends AbstractController
@@ -22,20 +26,49 @@ class ProductsController extends AbstractController
     }
 
     #[Route('/new', name: 'app_products_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, ProductsRepository $productsRepository): Response
+    public function new (Request $request, ProductsRepository $productsRepository, SluggerInterface $slugger): Response
     {
         $product = new Products();
         $form = $this->createForm(ProductsType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('img')->getData();
+            $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename . '-' . uniqid();
+            // Move the file to the directory where brochures are stored
+            //Cloudinary Api
+            try {
+                $image->move(
+                    $this->getParameter('img_path'),
+                    $newFilename
+                );
+                $product->setImg("https://res.cloudinary.com/dru9twkzb/image/upload/".$newFilename);
+                // ... handle exception if something happens during file upload
+            } catch (FileException $e) {
+                dd($e);
+            }
             $productsRepository->save($product, true);
-
+            $cloudinary = new Cloudinary(
+                [
+                    'cloud' => [
+                        'cloud_name' => 'dru9twkzb',
+                        'api_key' => '457992937569669',
+                        'api_secret' => 'qobHxRGhy2DrUi6RIbZKbtPguB4',
+                    ],
+                ]
+            );
+            $cloudinary->uploadApi()->upload(
+                $this->getParameter('img_path') . '/' . $newFilename,
+                ['public_id' => $newFilename]
+            );
+            $cloudinary->image($image)->resize(Resize::fill(100, 150))->toUrl();
             return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
         }
-
         return $this->renderForm('products/new.html.twig', [
-            'product' => $product,
+            'products' => $product,
             'form' => $form,
         ]);
     }
@@ -49,14 +82,44 @@ class ProductsController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_products_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Products $product, ProductsRepository $productsRepository): Response
+    public function edit(Request $request, Products $product, ProductsRepository $productsRepository, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(ProductsType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('img')->getData();
+            $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename . '-' . uniqid();
+            // Move the file to the directory where brochures are stored
+            //Cloudinary Api
+            try {
+                $image->move(
+                    $this->getParameter('img_path'),
+                    $newFilename
+                );
+                $product->setImg("https://res.cloudinary.com/dru9twkzb/image/upload/".$newFilename);
+                // ... handle exception if something happens during file upload
+            } catch (FileException $e) {
+                dd($e);
+            }
             $productsRepository->save($product, true);
-
+            $cloudinary = new Cloudinary(
+                [
+                    'cloud' => [
+                        'cloud_name' => 'dru9twkzb',
+                        'api_key' => '457992937569669',
+                        'api_secret' => 'qobHxRGhy2DrUi6RIbZKbtPguB4',
+                    ],
+                ]
+            );
+            $cloudinary->uploadApi()->upload(
+                $this->getParameter('img_path') . '/' . $newFilename,
+                ['public_id' => $newFilename]
+            );
+            $cloudinary->image($image)->resize(Resize::fill(100, 150))->toUrl();
             return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -66,13 +129,15 @@ class ProductsController extends AbstractController
         ]);
     }
 
+
     #[Route('/{id}', name: 'app_products_delete', methods: ['POST'])]
     public function delete(Request $request, Products $product, ProductsRepository $productsRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('_token'))) {
             $productsRepository->remove($product, true);
         }
 
         return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
     }
+
 }
