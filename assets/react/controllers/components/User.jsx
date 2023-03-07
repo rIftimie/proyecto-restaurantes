45ca43fed7f801899f8312ec878cfba9387d028e
jose carlos fernandez molina <josecarlosgoku7@gmail.com>
2023-03-07 16:27:49 +0100

    Prueba User

--- assets/react/controllers/components/User.jsx
+++ assets/react/controllers/components/User.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { getUsers } from "../../Api/Users";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const User = () => {
 
