dc015ce32cc555b6f332b2807487ab6f629a0673
jose carlos fernandez molina <josecarlosgoku7@gmail.com>
2023-03-07 19:42:32 +0100

    Probando cosas

--- assets/react/controllers/components/User.jsx
+++ assets/react/controllers/components/User.jsx
@@ -4,11 +4,36 @@ import { getUsers } from "../Api/Users";
 const User = () => {
 
     const [users, setUsers] = useState([]);
-    useEffect(() => {
-        getUsers()
-            .then((json) => setUsers(json));
-    }, []);
+    
+    // useEffect(() => {
+    //     getUsers()
+    //         .then((json) => setUsers(json))
+    // }, []);
 
+    useEffect(() => {
+        fetchDatos();
+    },[]);
+    
+    const fetchDatos = async () => {
+
+    
+        try {
+            const respuesta = await fetch(
+              getUsers()
+            );
+            const data = await respuesta.json();
+            
+            setUsers(data);
+            console.log(users);
+
+
+          } catch (error) {
+            console.log("Error: " + error);
+          }
+
+    };
+
+    
     return (
         <div>
             {users.map((user) => (
@@ -17,7 +42,7 @@ const User = () => {
                     <p>{user.email}</p>
                 </div>
             ))}
-            <p>{users}</p>
+            {/* <p>{users}</p> */}
         </div>
     );
 };
