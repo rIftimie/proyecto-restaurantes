d4818e1339f9e84d9a9a2e95691481288e545543
Brinqui <arincon2709@gmail.com>
2023-03-10 20:58:49 +0100

    Cambios componente Products+CSS #7

--- assets/react/controllers/components/products/Products.jsx
+++ assets/react/controllers/components/products/Products.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import PropTypes from 'prop-types';
 import ProductsButtons from './ProductsButtons';
@@ -26,5 +27,81 @@ Products.defaultProps = {
   texto: 'Nachos con queso.........................................3$',
   descripcion: 'Tortitas de trigo en forma de triangulo con carne picada y queso fresco',
 }
+=======
+import React, { useEffect, useState } from "react";
+import "../../pages/client/css/Products.css";
+import { getProducts } from "../../helpers/products";
+import ProductsButtons from "./ProductsButtons";
+
+const Products = ({
+  idres,
+  idtable,
+  orderProducts,
+  setOrderProducts,
+  setShow,
+}) => {
+  const [prods, setProds] = useState([]);
+  useEffect(() => {
+    getProds(idres);
+  }, []);
+
+  const getProds = async (el) => {
+    const prt = await getProducts(el);
+    setProds(prt);
+    setShow(true);
+  };
+
+  const icons={
+    'Gluten':<i class="fa-light fa-wheat"></i>,
+    'Lacteos':<i class="fa-light fa-glass"></i>,
+    'Frutos Secos':<i class="fa-light fa-peanuts"></i>,
+    'Huevos':<i class="fa-light fa-egg"></i>,
+}
+  console.log(icons);
+
+  return (
+    <>
+      {prods.map((prod) => (
+        <>
+          {prod.stock && !prod.hidden && (
+            <div key={prod.id} className="row my-5">
+              <div className="col-2 mx-2">
+                <img
+                  src={prod.product.img}
+                  alt={prod.product.name}
+                  width={200}
+                />
+              </div>
+              <div className="col-9">
+                <div className="row">
+                  <div className="col-9 border-dotted">
+                    <h5> {prod.product.name} </h5>
+                  </div>
+                  <div className="col border-dotted">
+                    <h5 className="text-end"> {prod.product.price}€ </h5>
+                  </div>
+                </div>
+                <h6> {prod.product.description} </h6>
+                {prod.product.allergens && (
+                  <h6 className="px-2">
+                    Alérgenos: {prod.product.allergens.map((all) => icons[all])}
+                  </h6>
+                )}
+                <ProductsButtons
+                  idres={idres}
+                  idtable={idtable}
+                  orderProducts={orderProducts}
+                  setOrderProducts={setOrderProducts}
+                  idprod={prod.id}
+                />
+              </div>
+            </div>
+          )}
+        </>
+      ))}
+    </>
+  );
+};
+>>>>>>> Stashed changes
 
 export default Products;
--- assets/react/controllers/pages/client/ClientPage.jsx
+++ assets/react/controllers/pages/client/ClientPage.jsx
@@ -2,14 +2,75 @@ import React, { useState } from "react";
 import PayButton from "../../components/stripe/PayButton";
 import Header from "../../components/products/Header";
 import Products from "../../components/products/Products";
+<<<<<<< Updated upstream
 import "../../App.css"
+=======
+import "../../App.css";
+import { postOrder } from "../../helpers/orders";
+
+const initialStateOrder = {
+  waiter_id: 1,
+  restaurant_id: 1,
+  table_order_id: 1,
+  status: 0,
+};
+const ClientPage = ({ idres, idtable }) => {
+  const [order, setOrder] = useState({});
+  const [orderProducts, setOrderProducts] = useState([]);
+  const [charge, setCharge] = useState(true);
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const orderCopy = { ...order };
+    orderCopy.restaurant_id = idres;
+    orderCopy.table_order_id = idtable;
+    setOrder(orderCopy);
+  }, []);
+
+  const handleClickPay = async () => {
+    if (orderProducts.length) {
+      setCharge(false);
+      await postOrder(orderProducts)
+        .then((res) => {
+          window.location.href =
+            "http://localhost:8000/orders/pay/" + res[0].id;
+        })
+        .catch((error) => {
+          setCharge(true);
+          console.log(error);
+        });
+    }
+  };
+>>>>>>> Stashed changes
 
 const ClientPage = () => {
   return (
     <div>
+<<<<<<< Updated upstream
       <Header />
       <Products />
       <PayButton />
+=======
+      {charge && show && <Header />}
+      {charge && (
+        <Products
+          idres={idres}
+          idtable={idtable}
+          orderProducts={orderProducts}
+          setOrderProducts={setOrderProducts}
+          setShow={setShow}
+        />
+      )}
+      {charge && show && (
+        <button
+          type="button"
+          className="btn btn-outline-success fw-bold m-3"
+          onClick={handleClickPay}
+        >
+          Pagar
+        </button>
+      )}
+      {!show && charge && <p>Loading...</p>}
+>>>>>>> Stashed changes
     </div>
   );
 };
