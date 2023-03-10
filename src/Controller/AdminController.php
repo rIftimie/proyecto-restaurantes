75c00ae2ce4b1a000347bc1440b84b24c9ebf123
jose carlos fernandez molina <josecarlosgoku7@gmail.com>
2023-03-10 17:20:47 +0100

    Haciendo Orders #20

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
+use App\Repository\OrdersRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -23,7 +24,7 @@ class AdminController extends AbstractController
     }
 
     #[Route('/user', name: 'app_user')]
-    public function indexUser(UserRepository $userRepository): Response
+    public function getUser(UserRepository $userRepository): Response
     {
       
         $users = $userRepository->findAll();
@@ -42,5 +43,28 @@ class AdminController extends AbstractController
         
         }
         return new JsonResponse($usersJSON);
+    }
+
+    #[Route('/orders', name: 'app_orders')]
+    public function getOrders(OrdersRepository $ordersRepository): Response
+    {
+      
+        $orders = $ordersRepository->findAll();
+        
+        $ordersJSON = [];
+
+        foreach ($orders as $order) {
+
+            $ordersJSON[] = array(
+
+                'id'=> $order->getId(),
+                'status' => $order->getStatus(),
+                'order_date' => $order->getOrderDate(),
+                'note' => $order->getNote(),
+
+            );
+        
+        }
+        return new JsonResponse($ordersJSON);
     }
 }
